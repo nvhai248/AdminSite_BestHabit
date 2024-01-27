c799638b0f01f9238cd8aacbfba53d59b9e2dbd9
Hải Nguyễn <nvhai2408@gmail.com>
2024-01-27 17:33:00 +0700

    done dashboard

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,78 +1,206 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./dashboard.css";
+import { useAuth } from "../../provider/authContext";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import axiosInstance from "../../configs/axios.config";
+
+const NewOptions = (year: string) => {
+  return {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: `Statistics on the number of users, habits, tasks and challenges created in ${year}`,
+      },
+    },
+  };
+};
+
+const NewData = (statisticalData: any) => {
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const dataSetHabit = new Array();
+  const dataSetTask = new Array();
+  const dataSetUser = new Array();
+  const dataSetChallenge = new Array();
+
+  for (let i = 0; i < 12; i++) {
+    try {
+      dataSetHabit.push(statisticalData.elements[i].habit_count);
+    } catch (error) {
+      dataSetHabit.push(0);
+    }
+
+    try {
+      dataSetTask.push(statisticalData.elements[i].task_count);
+    } catch (error) {
+      dataSetTask.push(0);
+    }
+
+    try {
+      dataSetUser.push(statisticalData.elements[i].user_count);
+    } catch (error) {
+      dataSetUser.push(0);
+    }
+
+    try {
+      dataSetChallenge.push(statisticalData.elements[i].challenge_count);
+    } catch (error) {
+      dataSetChallenge.push(0);
+    }
+  }
+
+  return {
+    labels,
+    datasets: [
+      {
+        label: "Habit",
+        data: dataSetHabit,
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+      {
+        label: "Task",
+        data: dataSetTask,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "User",
+        data: dataSetUser,
+        backgroundColor: "rgba(75, 192, 192, 0.5)",
+      },
+      {
+        label: "Challenge",
+        data: dataSetChallenge,
+        backgroundColor: "rgba(255, 206, 86, 0.5)",
+      },
+    ],
+  };
+};
 
 const Dashboard: React.FC = () => {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const [statisticalData, setStatisticalData] = useState<any>({});
+  const { token } = useAuth();
+  const [yearQuery, setYearQuery] = useState<number>(new Date().getFullYear());
+  const [optionsChart, setOptionsChart] = useState<any>({});
+  const [dataChart, setDataChart] = useState<any>({});
+
+  useEffect(() => {
+    axiosInstance
+      .get(`/statistical?year=${yearQuery}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.data && response.data.data) {
+          setStatisticalData(response.data.data);
+          let option = NewOptions(yearQuery.toString());
+          setOptionsChart(option);
+          let data = NewData(response.data.data);
+          setDataChart(data);
+
+          console.log(dataChart, optionsChart);
+        } else {
+          console.error("Invalid response format");
+        }
+      })
+      .catch((error) => console.error(error));
+  }, [yearQuery, token]);
+
   return (
     <main>
       <div className="header">
         <div className="left">
           <h1>Dashboard</h1>
         </div>
+        <div className="inputTime">
+          <div className="bx bx-search-alt"></div>
+          <input
+            type="number"
+            placeholder="year"
+            value={yearQuery}
+            onChange={(e) => setYearQuery(parseInt(e.target.value))}
+          />
+        </div>
       </div>
-      <Insights />
-      <div className="bottom-data">
-        <RecentOrders />
-      </div>
-    </main>
-  );
-};
-
-const Insights: React.FC = () => {
-  return (
-    <ul className="insights">
       <ul className="insights">
         <li>
-          <i className="bx bx-calendar-check"></i>
+          <i className="bx bx-user-circle"></i>
           <span className="info">
-            <h3>1,074</h3>
-            <p>Tasks</p>
+            <h3>{statisticalData.user_count}</h3>
+            <p>User</p>
+          </span>
+        </li>
+
+        <li>
+          <i className="bx bx-task"></i>
+          <span className="info">
+            <h3>{statisticalData.task_count}</h3>
+            <p>Task</p>
+          </span>
+        </li>
+
+        <li>
+          <i className="bx bx-calendar-heart"></i>
+          <span className="info">
+            <h3>{statisticalData.habit_count}</h3>
+            <p>Habit</p>
+          </span>
+        </li>
+
+        <li>
+          <i className="bx bx-run"></i>
+          <span className="info">
+            <h3>{statisticalData.challenge_count}</h3>
+            <p>Challenge</p>
           </span>
         </li>
       </ul>
-    </ul>
-  );
-};
-
-const RecentOrders: React.FC = () => {
-  return (
-    <div className="orders">
-      <div className="orders">
-        <div className="header">
-          <i className="bx bx-receipt"></i>
-          <h3>Recent Orders</h3>
-          <i className="bx bx-filter"></i>
-          <form action="#">
-            <div className="form-input">
-              <input type="search" placeholder="Search..." />
-              <button className="search-btn" type="submit">
-                <i className="bx bx-search"></i>
-              </button>
-            </div>
-          </form>
+      <div className="bottom-data">
+        <div className="chart">
+          <h2>Statistical</h2>
+          {dataChart.datasets ? (
+            <Bar options={optionsChart} data={dataChart} />
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>User</th>
-              <th>Order Date</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <img src="images/profile-1.jpg" alt="User" />
-                <p>John Doe</p>
-              </td>
-              <td>14-08-2023</td>
-              <td>
-                <span className="status completed">Completed</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-    </div>
+    </main>
   );
 };
 
--- src/pages/user/userPage.tsx
+++ src/pages/user/userPage.tsx
@@ -1,7 +1,61 @@
 import React from "react";
 
 const UserPage: React.FC = () => {
-  return <h1>User Content</h1>;
+  return (
+    <main>
+      <div className="header">
+        <div className="left">
+          <h1>Users</h1>
+        </div>
+      </div>
+      <div className="bottom-data">
+        <RecentOrders />
+      </div>
+    </main>
+  );
+};
+
+const RecentOrders: React.FC = () => {
+  return (
+    <div className="orders">
+      <div className="orders">
+        <div className="header">
+          <i className="bx bx-receipt"></i>
+          <h3>Recent Orders</h3>
+          <i className="bx bx-filter"></i>
+          <form action="#">
+            <div className="form-input">
+              <input type="search" placeholder="Search..." />
+              <button className="search-btn" type="submit">
+                <i className="bx bx-search"></i>
+              </button>
+            </div>
+          </form>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Order Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img src="images/profile-1.jpg" alt="User" />
+                <p>John Doe</p>
+              </td>
+              <td>14-08-2023</td>
+              <td>
+                <span className="status completed">Completed</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default UserPage;
